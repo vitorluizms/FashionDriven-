8627c46487c53c29e3b7feb132383b6ca24daf21
Vitor Luiz <vitor.luiz.eer@gmail.com>
2023-04-27 10:59:03 -0300

    Projeto parcialmente completo

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ while (user.length > "24" || user === "") {
   user = prompt("Digite um nome válido:");
 }
 
-
 const showUser = () => {
   const rightHeader = document.querySelector(".rightHeader");
   rightHeader.innerHTML += `
@@ -13,21 +12,25 @@ const showUser = () => {
 
 const x = document.querySelector(".linkImg");
 const selectItem = (item, tipo) => {
+  const x = document.querySelector(".linkImg");
   const itemSelected = document.querySelector(`${tipo} .itemSelected`);
   if (itemSelected !== null) {
     itemSelected.classList.remove("itemSelected");
   }
   item.classList.add("itemSelected");
 
-  confirmBtn(`.model`, `.collar`, `.tissue`);
-  
   const model = document.querySelector(".model .itemSelected");
+  console.log(model);
   const collar = document.querySelector(".collar .itemSelected");
+  console.log(collar);
   const tissue = document.querySelector(".tissue .itemSelected");
-  if (model !== null && collar !== null && tissue !== null && x.value !== '') {
-    sendBody(model.id, collar.id, tissue.id)
+  console.log(tissue);
+  if (model !== null && collar !== null && tissue !== null) {
+    confirmBtn(model, collar, tissue);
   }
-  console.log('oi')
+  if (model !== null && collar !== null && tissue !== null && x.value !== "") {
+    sendBody(model.id, collar.id, tissue.id, model, collar, tissue);
+  }
 };
 
 const sendBody = (model, collar, tissue) => {
@@ -35,100 +38,146 @@ const sendBody = (model, collar, tissue) => {
   let collarType;
   let tissueType;
   if (model == 1) {
-    modelType = 't-shirt'
+    modelType = "t-shirt";
   }
   if (model == 2) {
-    modelType = 'top-tank'
+    modelType = "top-tank";
   }
   if (model == 3) {
-    modelType = 'long'
+    modelType = "long";
   }
   if (collar == 4) {
-    collarType = 'v-neck'
+    collarType = "v-neck";
   }
   if (collar == 5) {
-    collarType = 'round'
+    collarType = "round";
   }
   if (collar == 6) {
-    collarType = 'polo'
+    collarType = "polo";
   }
   if (tissue == 7) {
-    tissueType = 'silk'
+    tissueType = "silk";
   }
   if (tissue == 8) {
-    tissueType = 'cottom'
+    tissueType = "cotton";
   }
   if (tissue == 9) {
-    tissueType = 'polyester'
+    tissueType = "polyester";
   }
-  const input = document.querySelector('.linkImg')
-  const inputValue = input.value
-  
+  const input = document.querySelector(".linkImg");
+  const inputValue = input.value;
+
   let body = {
-    'model': modelType,
-    'neck': collarType,
-    'material': tissueType,
-    'image': inputValue,
-    'owner': user,
-    'author': user
+    model: modelType,
+    neck: collarType,
+    material: tissueType,
+    image: inputValue,
+    owner: user,
+    author: user,
   };
-  sendPromise(body)
+  sendPromise(body);
 };
 
 const sendPromise = (body) => {
-  const promise = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', body)
-  // promise.then(successPromise)
-}
+  console.log(body);
+  const promise = axios.post(
+    "https://mock-api.driven.com.br/api/v4/shirts-api/shirts",
+    body
+  );
+  promise.then(successPromise);
+  promise.catch(errorPromise);
+};
+
+const errorPromise = () => {
+  const leftHidden = document.querySelectorAll(".shirts");
+  const buttonBlue = document.querySelector(".confirmBtn1");
+  buttonBlue.classList.add("hidden");
+  for (let i = 0; i < leftHidden.length; i++) {
+    leftHidden[i].classList.add("hidden");
+  }
+  const referenceHidden = document.querySelector(".reference");
+  const buttonHidden = document.querySelector(".confirmBtn");
+  referenceHidden.classList.add("hidden");
+  buttonHidden.classList.add("hidden");
+  const errorContent = document.querySelector(".leftContent");
+
+  errorContent.innerHTML += `
+  <div class = "errorContent">
+    <h5>Algo deu errado</h5>
+    <h6>Um passarinho me contou que a</h6>
+    <h6>imagem não é válida. Tente novamente!</h6>
+    <img src="./imagens/image1.svg" alt="errrorImage">
+    <h6>Voltando para a página</h6>
+    <h6>principal em 10s</h6>
+  </div>`;
+
+  setTimeout(finishError, 10000);
+};
 
 const successPromise = () => {
-  const leftHidden = document.querySelectorAll('.shirts')
-  const buttonBlue = document.querySelector('.confirmBtn1')
-  buttonBlue.classList.add('hidden')
-  for (let i=0; i<leftHidden.length; i++){
-    leftHidden[i].classList.add('hidden')
+  const leftHidden = document.querySelectorAll(".shirts");
+  const buttonBlue = document.querySelector(".confirmBtn1");
+  buttonBlue.classList.add("hidden");
+  for (let i = 0; i < leftHidden.length; i++) {
+    leftHidden[i].classList.add("hidden");
   }
-  const referenceHidden = document.querySelector('.reference')
-  const buttonHidden = document.querySelector('.confirmBtn')
-  referenceHidden.classList.add('hidden')
-  buttonHidden.classList.add('hidden')
-  const successContent = document.querySelector('.leftContent')
-  
+  const referenceHidden = document.querySelector(".reference");
+  const buttonHidden = document.querySelector(".confirmBtn");
+  referenceHidden.classList.add("hidden");
+  buttonHidden.classList.add("hidden");
+  const successContent = document.querySelector(".leftContent");
+
   successContent.innerHTML += `
     <div class="successContent">
       <h5>Pedido feito com sucesso!</h5>
-      <img src="./imagens/Seda.png" alt="">
+      <img src="./imagens/Seda.png" alt="sucessImage">
       <h6>Voltando para a página</h6>
       <h6> principal em 10s</h6>
-    </div>`
+    </div>`;
   setTimeout(finishSuccess, 10000);
-}
+};
 
 const finishSuccess = () => {
-  const successHidden = document.querySelector('.successContent').remove()
-  const leftHidden = document.querySelectorAll('.shirts')
-  for (let i=0; i<leftHidden.length; i++){
-    leftHidden[i].classList.remove('hidden')
+  getPromise();
+  const successHidden = document.querySelector(".successContent").remove();
+  const leftHidden = document.querySelectorAll(".shirts");
+  for (let i = 0; i < leftHidden.length; i++) {
+    leftHidden[i].classList.remove("hidden");
   }
-  const referenceHidden = document.querySelector('.reference')
-  const buttonHidden = document.querySelector('.confirmBtn')
-  referenceHidden.classList.remove('hidden')
-  buttonHidden.classList.remove('hidden')
+  const referenceHidden = document.querySelector(".reference");
+  const buttonHidden = document.querySelector(".confirmBtn");
+  referenceHidden.classList.remove("hidden");
+  buttonHidden.classList.remove("hidden");
   // location.reload()
-}
+};
+
+const finishError = () => {
+  const successHidden = document.querySelector(".errorContent").remove();
+  const leftHidden = document.querySelectorAll(".shirts");
+  for (let i = 0; i < leftHidden.length; i++) {
+    leftHidden[i].classList.remove("hidden");
+  }
+  const referenceHidden = document.querySelector(".reference");
+  const buttonHidden = document.querySelector(".confirmBtn");
+  referenceHidden.classList.remove("hidden");
+  buttonHidden.classList.remove("hidden");
+  // selectItem()
+  // location.reload()
+};
 
 const confirmBtn = (tipo1, tipo2, tipo3) => {
-  console.log('ola')
   const x = document.querySelector(".linkImg");
   x.addEventListener("input", (event) => {
-    console.log('a')
-    const grupo1 = document.querySelector(`${tipo1} .itemSelected`);
-    console.log(grupo1)
-    const grupo2 = document.querySelector(`${tipo2} .itemSelected`);
-    console.log(grupo2)
-    const grupo3 = document.querySelector(`${tipo3} .itemSelected`);
-    console.log(grupo3)
-    console.log(event.target.value)
-    if (grupo1 && grupo2 && grupo3 && event.target.value !== undefined) {
+    console.log(tipo1);
+    console.log(tipo2);
+    console.log(tipo3);
+    console.log(event.target.value);
+    if (
+      tipo1 !== null &&
+      tipo2 !== null &&
+      tipo3 !== null &&
+      event.target.value !== undefined
+    ) {
       const add = document.querySelector(".confirmBtn1");
       add.classList.add("confirmSelected");
       add.classList.remove("hidden");
@@ -136,8 +185,12 @@ const confirmBtn = (tipo1, tipo2, tipo3) => {
       remove.classList.add("hidden");
     }
     if (
-      (grupo1 && grupo2 && grupo3 && event.target.value === undefined) ||
-      event.target.value === "") {
+      tipo1 === null ||
+      tipo2 === null ||
+      tipo3 === null ||
+      event.target.value === undefined ||
+      event.target.value === ""
+    ) {
       const add1 = document.querySelector(".confirmBtn1");
       add1.classList.remove("confirmSelected");
       add1.classList.add("hidden");
@@ -152,6 +205,7 @@ const getPromise = () => {
     "https://mock-api.driven.com.br/api/v4/shirts-api/shirts"
   );
   promise.then(renderModels);
+  console.log(promise)
 
   const searchSelected = document.querySelector(".filterSelected");
   if (searchSelected !== null) {
@@ -166,13 +220,14 @@ const renderModels = (allModels) => {
   let models;
   models = document.querySelector(".shirtArea");
   arrayModels = allModels.data;
+  console.log(arrayModels)
   models.innerHTML = "";
   for (let i = 0; i < arrayModels.length; i++) {
     models.innerHTML += `
-        <div class="models" name="${arrayModels[i].model}">
+        <button onclick= "readyProduct('${arrayModels[i].material}', '${arrayModels[i].neck}', '${arrayModels[i].model}', '${arrayModels[i].image}', '${arrayModels[i].owner}')" class="models" name="${arrayModels[i].model}">
             <img src="${arrayModels[i].image}" alt="image">
             <p><strong>Criador:</strong> ${arrayModels[i].owner}</p>
-        </div>`;
+        </button>`;
   }
 };
 
@@ -198,10 +253,10 @@ const filterTshirt = (tShirts) => {
   for (let i = 0; i < arrayModels.length; i++) {
     if (arrayModels[i].model === "t-shirt") {
       models.innerHTML += `
-            <div class="models" name="${arrayModels[i].model}">
+            <button onclick= "readyProduct('${arrayModels[i].material}', '${arrayModels[i].neck}', '${arrayModels[i].model}', '${arrayModels[i].image}', '${arrayModels[i].owner}')" class="models" name="${arrayModels[i].model}">
                 <img src="${arrayModels[i].image}" alt="image">
                 <p><strong>Criador:</strong> ${arrayModels[i].owner}</p>
-            </div>`;
+            </button>`;
     }
   }
 };
@@ -228,10 +283,10 @@ const filterShirt = (tShirts) => {
   for (let i = 0; i < arrayModels.length; i++) {
     if (arrayModels[i].model === "top-tank") {
       models.innerHTML += `
-            <div class="models" name="${arrayModels[i].model}">
+            <button onclick= "readyProduct('${arrayModels[i].material}', '${arrayModels[i].neck}', '${arrayModels[i].model}', '${arrayModels[i].image}', '${arrayModels[i].owner}')" class="models" name="${arrayModels[i].model}">
                 <img src="${arrayModels[i].image}" alt="image">
                 <p><strong>Criador:</strong> ${arrayModels[i].owner}</p>
-            </div>`;
+            </button>`;
     }
   }
 };
@@ -258,13 +313,20 @@ const filterLong = (tShirts) => {
   for (let i = 0; i < arrayModels.length; i++) {
     if (arrayModels[i].model === "long") {
       models.innerHTML += `
-            <div class="models" name="${arrayModels[i].model}">
+            <button onclick= "readyProduct('${arrayModels[i].material}', '${arrayModels[i].neck}', '${arrayModels[i].model}', '${arrayModels[i].image}', '${arrayModels[i].owner}')" class="models" name="${arrayModels[i].model}">
                 <img src="${arrayModels[i].image}" alt="image">
                 <p><strong>Criador:</strong> ${arrayModels[i].owner}</p>
-            </div>`;
+            </button>`;
     }
   }
 };
+
+const readyProduct = (material, neck, model, image, author) => {
+  console.log(material)
+  console.log(neck)
+  console.log(image)
+  console.log(model)
+  console.log(author)
+}
 showUser();
 getPromise();
-successPromise()
